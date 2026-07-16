e79952e9ec7799643ca30a364e8a55746b9caf88
Rusetska Tetiana <rusetskatata@gmail.com>
2026-07-16 09:42:27 +0200

    vorlesung

---
+++ app/vorlesung/cl_components/Pokemon.jsx
@@ -0,0 +1,19 @@
+"use client";
+
+export default async function Pokemon() {
+    const response = await fetch('https://pokeapi.co/api/v2/pokemon');
+    const { results: pokemon } = await response.json();
+
+    // Simuliere langsame UI.
+    await new Promise((resolve) => {
+        setTimeout(resolve, 4000);
+    });
+
+    return (
+        <div className="flex flex-col items-center justify-center">
+            {pokemon?.map((p) => (
+                <p key={p.name}>{p.name}</p>
+            ))}
+        </div>
+    );
+}
---
+++ app/vorlesung/page.jsx
@@ -0,0 +1,18 @@
+import { Suspense } from 'react';
+import Pokemon from './cl_components/Pokemon';
+
+export default async function PokemonPage() {
+    return (
+        <main>
+            <Suspense
+                fallback={
+                    <p className="text-orange-600 text-2xl font-bold">
+                        Loading pokemon...
+                    </p>
+                }
+            >
+                <Pokemon />
+            </Suspense>
+        </main>
+    )
+}
